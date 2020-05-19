099871d64a4c572b4b2f59ed643e948e0f734e3c
Quirinux GNU/Linux <49002514+quirinux-so@users.noreply.github.com>
2020-05-19 12:55:08 +0200

    Programa em português

--- Lenguajes/quirinux-kernel-pt.sh
+++ Lenguajes/quirinux-kernel-pt.sh
@@ -38,10 +38,10 @@ sudo apt-get install -f
 # INSTALAR O KERNEL AVL
 
 echo "# Download Kernel"; sleep 1s
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YX99kSMorEWKgTnypg-cVKzF4sFXdIGf' -O linux-image-5.4.28avl2-lowlatency.deb
+wget --no-check-certificate 'https://kernelavl.quirinux.org' -O linux-image-5.4.28avl2-lowlatency.deb
 
 echo "# Dowload Headers"; sleep 1s
-wget  --no-check-certificate 'https://docs.google.com/uc?export=download&id=18YiaU1zTjCzRvZKBRiQCl078LLaQVFoq' -O linux-headers-5.4.28avl2-lowlatency.deb
+wget  --no-check-certificate 'https://headersavl.quirinux.org' -O linux-headers-5.4.28avl2-lowlatency.deb
 
 echo "# Instalando o novo kernel"; sleep 1s
 sudo dpkg -i linux-headers-5.4.28avl2-lowlatency.deb linux-image-5.4.28avl2-lowlatency.deb
